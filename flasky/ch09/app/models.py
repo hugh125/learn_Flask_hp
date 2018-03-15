9a8d93dddf399232e725ea1d9809eafc6f4e78f3
hugh <hugh182@qq.com>
2018-03-15 16:41:51 +0800

    push_log

--- flasky/ch09/app/models.py
+++ flasky/ch09/app/models.py
@@ -21,13 +21,14 @@ class Role(db.Model):
 	__tablename__ = 'roles'	# 数据库里的表名
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
-	default = db.Column(db.Boolean, default=False, index=True)
-	permissions = db.Column(db.Integer)
+	#default = db.Column(db.Boolean, default=False, index=True)
+	##permissions = db.Column(db.Integer)
 	#users = db.relationship('User', backref='role', lazy='dynamic')
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
 
+	'''
 	@staticmethod
 	def insert_roles():
 		roles = {
@@ -48,7 +49,7 @@ class Role(db.Model):
 			role.default = roles[r][1]
 			db.session.add(role)
 		db.session.commit()
-
+'''
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
