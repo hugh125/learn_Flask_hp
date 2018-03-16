0819bd9190a0d6f577686fcde557b3a95f01743d
hugh <hugh182@qq.com>
2018-03-16 10:26:43 +0800

    push_log

--- flasky/ch08/app/auth/forms.py
+++ flasky/ch08/app/auth/forms.py
@@ -9,19 +9,15 @@ from wtforms.validators import Required, Length, Email
 from wtforms import ValidationError
 from ..models import User
 
-class LoginForm(FlaskForm):
+class LoginForm(Form):
 	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
 	password = PasswordField('Password', validators=[Required()])
 	remember_me = BooleanField('Keep me logged in')
 	submit = SubmitField('Login_submit')
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(Form):
 	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	username = StringField('Username',
-						   validators=[Required(),
-									   EqualTo('^[A-Za-z0-9_.]*$', 0,
-																	   'Usernames must have only letters',
-											   'numbers, dots or underscores')])
+	username = StringField('Username', validators=[Required(), EqualTo('^[A-Za-z0-9_.]*$', 0, 'Usernames must have only letters', 'numbers, dots or underscores')])
 	password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Password must match.')])
 	password2 = PasswordField('Confirm password', validators=[Required()])
 	submit = SubmitField('Register')
