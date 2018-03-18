48132027c31b0224d8d0f94c02cc8a82b0c56016
hugh <hugh182@qq.com>
2018-03-19 07:52:22 +0800

    push_log

--- flasky/ch08/__init__.py
+++ flasky/ch08/__init__.py
@@ -1,5 +1,6 @@
 '''
 	https://github.com/miguelgrinberg/flasky/tree/8d
 	u = User(username='hugh', email='test.test.com', password='pwd1')
+	u2 = User(username='u2', email='u2.com', password='u2pwd2')
 
 '''
--- flasky/ch08/app/auth/forms.py
+++ flasky/ch08/app/auth/forms.py
@@ -1,30 +1,32 @@
 from flask_wtf import Form
-from flask_wtf import FlaskForm
+#from flask_wtf import FlaskForm
 from wtforms import \
 	StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import \
-	Required, Length, Email, Regexp, EqualTo
+	DataRequired, Length, Email, Regexp, EqualTo
 from wtforms.validators import Required, Length, Email
 
 from wtforms import ValidationError
 from ..models import User
 
 class LoginForm(Form):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	password = PasswordField('Password', validators=[Required()])
+	email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Keep me logged in')
 	submit = SubmitField('Login_submit')
 
+""""""
 class RegistrationForm(Form):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	username = StringField('Username', validators=[Required(), EqualTo('^[A-Za-z0-9_.]*$', 0, 'Usernames must have only letters', 'numbers, dots or underscores')])
-	password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Password must match.')])
-	password2 = PasswordField('Confirm password', validators=[Required()])
-	submit = SubmitField('Register')
+	pass
+	email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+	username = StringField('Username', validators=[DataRequired(), Regexp('^[A-Za-z0-9_.]*$', 0, 'Usernames must have only letters numbers, dots or ' 'underscores')])
+	password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Password must match.')])
+	# password2 = PasswordField('Confirm password', validators=[DataRequired()])
+	# submit = SubmitField('Register')
 
-	def validate_email(self, field):
-		if User.query.filter_by(email=field.data).first():
-			raise ValidationError('Email already registerd.')
-	def validate_username(self, field):
-		if User.query.filter_by(username=field.data).first():
-			raise ValidationError('Username already in use.')
+	# def validate_email(self, field):
+	# 	if User.query.filter_by(email=field.data).first():
+	# 		raise ValidationError('Email already registerd.')
+	# def validate_username(self, field):
+	# 	if User.query.filter_by(username=field.data).first():
+	# 		raise ValidationError('Username already in use.')
